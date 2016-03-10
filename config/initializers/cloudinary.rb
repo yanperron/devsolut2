2d2ae91ad528d061da4b1aebdfeec41cd898ae54
bach georges <ferrenbachg@hotmail.fr>
2016-03-10 12:42:29 +0100

    on a du taff

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'roritobach'
+  config.api_key = '823931488395973'
+  config.api_secret = 'j-pKqHfZZrNnMw3C4J9KevYY_2Q'
+  config.cdn_subdomain = true
+end
