00b4b89742534257b16a369343f442d975b286ad
yan <perronyannig@hotmail.com>
2016-03-10 13:58:08 +0100

    Add missing files

---
+++ app/admin/agency.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register Agency do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+ permit_params :name
+ permit_params :description
+ permit_params :repos_public
+ permit_params :total_stars
+ permit_params :photo
+
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+end
